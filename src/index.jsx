5a81fa1543e41bea99317fd7207013eab2caca3c
v.matokin <v.matokin@nodeart.io>
2019-05-17 18:58:05 +0300

    feat(test)

--- src/index.jsx
+++ src/index.jsx
@@ -9,3 +9,5 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae aliquid quas error sint libero commodi perferendis, reiciendis voluptatum, perspiciatis aspernatur earum officiis quasi non consequatur molestias? Quae, veritatis amet!
